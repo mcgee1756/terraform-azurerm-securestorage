5a19133bc760abb03b5f741c93a911b13c2d5df2
Mike McGee <mike@myndcorepartners.com>
2024-03-04 09:58:26 -0700

    tf files

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "resource_group_name" {
+  type        = string
+  description = "value"
+
+}
+variable "location" {
+  type        = string
+  description = "value"
+
+}
+variable "storage_account_name" {
+  type        = string
+  description = "value"
+
+}
+
+variable "environment" {
+  type        = string
+  description = "value"
+
+}
